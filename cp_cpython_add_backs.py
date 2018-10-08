687b2892f8522c84a6a82cf4af02a06823ec1431
TG-Techie <39284876+TG-Techie@users.noreply.github.com>
2018-10-07 22:00:13 -0400

    Update cp_cpython_add_backs.py

--- cp_cpython_add_backs.py
+++ cp_cpython_add_backs.py
@@ -1,4 +1,6 @@
-
+'''This is a module made to add currently missing features back to
+adafruit's circuit python. It is a poor substitue b/c it will take 
+up more memory and it isn't in the base firmware. i find it better than nothing'''
 
 def delattr(target,name):
     exec('del str_target.'+name, {'str_target':target})
